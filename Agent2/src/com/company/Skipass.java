b5adcadfc8212bec2bd7c9ebf988e879175fb925
Rustam Sleptsov <rustam.slep11@gmail.com>
2022-03-01 19:38:32 +0700

    Agent2

---
+++ Agent2/src/com/company/Skipass.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.io.*;
+
+public class Skipass {
+    char[] ski;
+    StringBuffer passes = new StringBuffer();
+
+    public void readFile() {
+        Skipass user = new Skipass();
+        try {
+            File file = new File("/Users/79243/Downloads/passes.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+
+               this.ski = line.toCharArray();
+               StringBuffer result = new StringBuffer();
+               user.newFormat(result, ski);
+
+                System.out.println(result);
+                passes.append(result);
+                passes.append(System.getProperty("line.separator"));
+                user.fileWriter(passes);
+                line = reader.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fileWriter(StringBuffer fff) {
+        try(FileWriter writer = new FileWriter("newSkipass.txt", false))
+        {
+            writer.write(String.valueOf(fff));
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void newFormat(StringBuffer result, char ski[]) {
+        int x = 14;
+        while (x > 7) {
+            result.append(ski[x]);
+            x++;
+            result.append(ski[x]);
+            x = x - 3;
+        }
+    }
+}
---
+++ Agent2/src/com/company/StartProgram.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class StartProgram {
+
+    public static void main(String[] args) {
+        Skipass start = new Skipass();
+        start.readFile();
+    }
+}
