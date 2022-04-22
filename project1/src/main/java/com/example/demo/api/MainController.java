0f5eeecdf255a4f5cde8bc5fb7269db6a35a1158
Rustam Sleptsov <rustam.slep11@gmail.com>
2022-04-22 18:17:11 +0700

    ggg

--- project1/src/main/java/com/example/demo/api/MainController.java
+++ project1/src/main/java/com/example/demo/api/MainController.java
@@ -6,11 +6,11 @@ import com.example.demo.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -24,9 +24,17 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -43,14 +51,5 @@ public class MainController {
 
         return "main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, @RequestParam String tag, Map<String, Object> model) {
-        List<Message> messages = messageRepository.findByTag(filter);
-
-        model.put("messages", messages);
-        return "main";
-    }
-
 
 }
